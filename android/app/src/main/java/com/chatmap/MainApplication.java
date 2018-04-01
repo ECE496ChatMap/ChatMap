a22ca4d8a5680acd3f6b958ab963481be6f5d8a6
ewonguoft <eltong.wong@mail.utoronto.ca>
2018-04-01 16:55:54 -0400

    adding volume

--- android/app/src/main/java/com/chatmap/MainApplication.java
+++ android/app/src/main/java/com/chatmap/MainApplication.java
@@ -3,6 +3,7 @@ package com.chatmap;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.ninty.system.setting.SystemSettingPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.arttitude360.reactnative.rngoogleplaces.RNGooglePlacesPackage;
 import com.facebook.react.ReactNativeHost;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new SystemSettingPackage(),
             new VectorIconsPackage(),
             new RNGooglePlacesPackage(),
           new MapsPackage()
